e6d49ecf778b7559689688b0e0aba04c38490250
Jake BS <j.bailey-saltmarsh+github@outlook.com>
2021-05-13 20:35:26 +0100

    added my files

---
+++ README.md
@@ -0,0 +1,28 @@
+# vscode-real-line-numbers
+
+vscode-real-line-numbers is a simple Visual Studio Code extension that adds __"real"__ line numbers to Visual Studio Code.
+
+This means it does not show line numbers for blank lines or lines that start with comments. This is extremly useful for projects where sourcecode is trimmed for whitespace and comments. 
+
+I created this extension to aid with writing my own **HACK ASM**, although it has many more uses!
+
+## Install
+
+Currently, I don't know how to publish this extension to the VSCode marketplace, so until then you will need to 
+- 
+- Select `Install Extension` and search for 'Relative line numbers'
+
+## Commands
+
+__Relative Line Numbers: Disable__ (`extension.relativeLineNumbersDisable`) - turn off relative line numbering. This is useful when debugging or in other situations where other gutter icons need to be visible.
+
+__Relative Line Numbers: Enable__ (`extension.relativeLineNumbersEnable`) - turn relative line numbering back on.
+
+Use `Control+Shift+P` to open the command palette and run the commands.
+
+## Source
+This repo is a fork of the following
+[Github](https://github.com/extr0py/vscode-relative-line-numbers)
+
+### License
+This project licensed under the **MIT License**
---
+++ out/src/extension.js
@@ -0,0 +1,69 @@
+'use strict';
+var vscode = require('vscode');
+var path = require("path");
+var MAX_ICONS = 99;
+function activate(context) {
+    let extconsole = vscode.window.createOutputChannel("Line Nums");
+    var decorations = createDecorations();
+    var isExtensionEnabled = true;
+    vscode.commands.registerCommand("extension.relativeLineNumbersEnable", function () {
+        isExtensionEnabled = true;
+        setRelativeLineDecorations();
+    });
+    vscode.commands.registerCommand("extension.relativeLineNumbersDisable", function () {
+        isExtensionEnabled = false;
+        clearRelativeLineDecorations();
+    });
+    vscode.window.onDidChangeTextEditorSelection(function (e) {
+        if (!isExtensionEnabled)
+            return;
+        setRelativeLineDecorations();
+    });
+    function setRelativeLineDecorations() {
+        var editor = vscode.window.activeTextEditor;
+        if (!editor)
+            return;
+        var selection = editor.selection;
+        
+        var line = editor.selection.active.line;
+        var totalLines = editor.document.lineCount;
+        var totalLinesAnnotated = 0;
+        for (var l = 0; l < totalLines || totalLinesAnnotated < 99; l++) {
+            var rangesForDecoration = [];
+            var text = editor.document.lineAt(l).text;
+            extconsole.appendLine(l);
+            extconsole.appendLine(text);
+            if (text.startsWith("//") || text.trim() === "") {
+                
+                continue;
+            }
+            rangesForDecoration.push(new vscode.Range(l, 0, l, 0));
+            editor.setDecorations(decorations[totalLinesAnnotated], rangesForDecoration);
+            totalLinesAnnotated++;
+        }
+    }
+    function clearRelativeLineDecorations() {
+        var editor = vscode.window.activeTextEditor;
+        if (!editor)
+            return;
+        decorations.forEach(function (d) {
+            editor.setDecorations(d, []);
+        });
+    }
+}
+exports.activate = activate;
+function createDecorations() {
+    var ret = [];
+    for (var i = 0; i < MAX_ICONS; i++) {
+        ret.push(vscode.window.createTextEditorDecorationType({
+            gutterIconPath: path.join(__dirname, "..", "..", "images", i.toString() + ".png"),
+            gutterIconSize: "cover",
+        }));
+    }
+    return ret;
+}
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
